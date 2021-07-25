397dfb1876de4780805dea9f7593f3c72f928c2e
Deshan Vimukthi <deshanvimukthi59@gmail.com>
2021-07-25 22:17:56 +0530

    Icon onClick

--- src/Components/Sidebar/index.js
+++ src/Components/Sidebar/index.js
@@ -12,7 +12,7 @@ import {
 function Sidebar({ isOpen, toggle }) {
   return (
     <SidebarContainer isOpen={isOpen} onClick={toggle}>
-      <Icon>
+      <Icon onClick={toggle}>
         <CloseIcon />
       </Icon>
       <SidebarMenu>
