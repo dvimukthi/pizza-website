f305ff475cdc1343a06e06d4e457e72ad902a625
Deshan Vimukthi <deshanvimukthi59@gmail.com>
2021-07-25 23:02:51 +0530

    import ProductsElements

--- src/Components/Products/index.js
+++ src/Components/Products/index.js
@@ -1,4 +1,16 @@
 import React from "react";
+import {
+  ProductsContainer,
+  ProductWrapper,
+  ProductsHeading,
+  ProductTitle,
+  ProductCard,
+  ProductImg,
+  ProductInfo,
+  ProductDesc,
+  ProductPrice,
+  ProductButton,
+} from "./ProductsElements";
 
 function products() {
   return (
@@ -9,6 +21,12 @@ function products() {
           return (
             <ProductCard key={index}>
               <ProductImg src={product.img} alt={product.alt} />
+              <ProductInfo>
+                <ProductTitle>{product.name}</ProductTitle>
+                <ProductDesc>{product.desc}</ProductDesc>
+                <ProductPrice>{product.price}</ProductPrice>
+                <ProductButton>{product.button}</ProductButton>
+              </ProductInfo>
             </ProductCard>
           );
         })}
