5933bfa9e04f5c30251cb425bfe23853de13ebf1
Deshan Vimukthi <deshanvimukthi59@gmail.com>
2021-07-25 22:27:23 +0530

    Products

---
+++ src/Components/Products/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function products() {
+  return (
+    <ProductsContainer>
+      <ProductsHeading>Heading</ProductsHeading>
+      <ProductWrapper></ProductWrapper>
+    </ProductsContainer>
+  );
+}
+
+export default products;
