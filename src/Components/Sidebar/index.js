7f0998c094b774f096243bd4ca47e7d243a23ff5
Deshan Vimukthi <deshanvimukthi59@gmail.com>
2021-07-25 20:41:01 +0530

    sidebar components

--- src/Components/Sidebar/index.js
+++ src/Components/Sidebar/index.js
@@ -1,7 +1,30 @@
 import React from "react";
+import {
+  SidebarContainer,
+  Icon,
+  CloseIcon,
+  SidebarMenu,
+  SidebarLink,
+  SidebarRoute,
+  SideBtnWrap,
+} from "./SidebarElements";
 
 function Sidebar() {
-  return <div></div>;
+  return (
+    <SidebarContainer>
+      <Icon>
+        <CloseIcon />
+      </Icon>
+      <SidebarMenu>
+        <SidebarLink to="/">Pizzas</SidebarLink>
+        <SidebarLink to="/">Desserts</SidebarLink>
+        <SidebarLink to="/">Full Menu</SidebarLink>
+      </SidebarMenu>
+      <SideBtnWrap>
+        <SidebarRoute to="/">Order Now</SidebarRoute>
+      </SideBtnWrap>
+    </SidebarContainer>
+  );
 }
 
 export default Sidebar;
