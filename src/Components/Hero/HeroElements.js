88e4da5947971755102be97b96ff900df1e1db0e
Deshan Vimukthi <deshanvimukthi59@gmail.com>
2021-07-25 19:26:10 +0530

    add background image

--- src/Components/Hero/HeroElements.js
+++ src/Components/Hero/HeroElements.js
@@ -2,6 +2,9 @@ import styled from "styled-components";
 import ImgBg from "../../images/pizza-3.jpg";
 
 export const HeroContainer = styled.div`
-  background: liner-gradient(to right, rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.1)),
-    url() (${ImgBg});
+  background: linear-gradient(to right, rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.1)),
+    url(${ImgBg});
+  height: 100vh;
+  background-position: center;
+  background-size: cover;
 `;
