887a2f70e44aca69fdd37a8f2171aa0dd85af527
Deshan Vimukthi <deshanvimukthi59@gmail.com>
2021-07-25 18:39:49 +0530

    globalstyle

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 import Navbar from "./Components/Navbar";
+import { GlobalStyle } from "./globalStyles";
 
 function App() {
   return (
     <Router>
+      <GlobalStyle />
       <Navbar />
     </Router>
   );
