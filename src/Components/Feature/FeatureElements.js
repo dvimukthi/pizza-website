752824100bf082de3bf7dda2bce969a9357c71d6
Deshan Vimukthi <deshanvimukthi59@gmail.com>
2021-07-26 20:17:56 +0530

    FeatureElements

---
+++ src/Components/Feature/FeatureElements.js
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+import Styled from "styled-components";
+import FeaturePic from "../../images/featured3.jpg";
+
+export const FeatureContainer = styled.div`
+  background: linear-gradient(to right, rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.1)),
+    url(${FeaturePic});
+  height: 100vh;
+  max-height: 500px;
+  background-position: center;
+  background-size: cover;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  color: #fff;
+  text-align: center;
+  padding: 0 1rem;
+`;
