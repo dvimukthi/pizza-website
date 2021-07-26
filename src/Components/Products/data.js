315d3538c9486d63dc3c32991d559e754c5e4533
Deshan Vimukthi <deshanvimukthi59@gmail.com>
2021-07-26 20:32:06 +0530

    productDataTwo

--- src/Components/Products/data.js
+++ src/Components/Products/data.js
@@ -28,3 +28,30 @@ export const productData = [
     button: "Add to Cart",
   },
 ];
+
+export const productDataTwo = [
+  {
+    img: product1,
+    alt: "Pizza",
+    name: "Supreme Pizza",
+    desc: "Marinara sauce, basil, italian sausage, roma tomatoes, olives and pesto",
+    price: "$19.99",
+    button: "Add to Cart",
+  },
+  {
+    img: product2,
+    alt: "Pizza",
+    name: "Hawaiian Paradise",
+    desc: "Marinara sauce, basil, italian sausage, roma tomatoes, olives and pesto",
+    price: "$16.99",
+    button: "Add to Cart",
+  },
+  {
+    img: product3,
+    alt: "Pizza",
+    name: "Veggie Overload",
+    desc: "Marinara sauce, basil, italian sausage, roma tomatoes, olives and pesto",
+    price: "$14.99",
+    button: "Add to Cart",
+  },
+];
