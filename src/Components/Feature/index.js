05b2aca00e8049cc06ecb6f0a706f01c86a0716a
Deshan Vimukthi <deshanvimukthi59@gmail.com>
2021-07-26 20:13:00 +0530

    Feature button

--- src/Components/Feature/index.js
+++ src/Components/Feature/index.js
@@ -1,9 +1,12 @@
 import React from "react";
+import { FeatureContainer, FeatureButton } from "./FeatureElements";
 
 function Feature() {
   return (
     <FeatureContainer>
       <h1>Pizza Of the Day</h1>
+      <p>Truffle Alfredo sauce topped with 24 carat gold dust.</p>
+      <FeatureButton>Order Now</FeatureButton>
     </FeatureContainer>
   );
 }
