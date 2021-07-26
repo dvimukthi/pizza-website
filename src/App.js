4849565abb91791d27c4b5c167f01941933b45ed
Deshan Vimukthi <deshanvimukthi59@gmail.com>
2021-07-26 20:42:01 +0530

    import app.js to productDataTwo

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router } from "react-router-dom";
 import Hero from "./Components/Hero";
 import { GlobalStyle } from "./globalStyles";
 import Products from "./Components/Products";
-import { productData } from "./Components/Products/data";
+import { productData, productDataTwo } from "./Components/Products/data";
 import Feature from "./Components/Feature";
 
 function App() {
@@ -13,6 +13,7 @@ function App() {
       <Hero />
       <Products heading="Choose your favorite" data={productData} />
       <Feature />
+      <Products heading="Sweet Treats for you" data={productDataTwo} />
     </Router>
   );
 }
