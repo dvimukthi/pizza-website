d838df2e1e8fc4129cae0dbd21706b2599a4a1b2
Deshan Vimukthi <deshanvimukthi59@gmail.com>
2021-07-26 20:39:44 +0530

    import sweet1, sweet2, sweet3

--- src/Components/Products/data.js
+++ src/Components/Products/data.js
@@ -1,6 +1,9 @@
 import product1 from "../../images/product-1.jpg";
 import product2 from "../../images/product-2.jpg";
 import product3 from "../../images/product-3.jpg";
+import sweet1 from "../../images/sweet3.jpg";
+import sweet2 from "../../images/sweet-2.jpg";
+import sweet3 from "../../images/sweet-3.jpg";
 
 export const productData = [
   {
