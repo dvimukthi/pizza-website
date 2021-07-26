06d4c24c1a306e6b245a6735277a7cc3f374659c
Deshan Vimukthi <deshanvimukthi59@gmail.com>
2021-07-26 20:26:44 +0530

    FeatureButton hover

--- src/Components/Feature/FeatureElements.js
+++ src/Components/Feature/FeatureElements.js
@@ -34,4 +34,11 @@ export const FeatureButton = styled.button`
   background: #ffc500;
   color: #000;
   transition: 0.2s ease-out;
+
+  &:hover {
+    color: #fff;
+    background: #e31837;
+    transition: 0.2s ease-out;
+    cursor: pointer;
+  }
 `;
