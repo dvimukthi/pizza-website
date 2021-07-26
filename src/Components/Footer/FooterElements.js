1fca6205b05a3ba34a707bb4a2cd9bfde04bd38f
Deshan Vimukthi <deshanvimukthi59@gmail.com>
2021-07-26 21:25:01 +0530

    FooterWrap

--- src/Components/Footer/FooterElements.js
+++ src/Components/Footer/FooterElements.js
@@ -4,3 +4,13 @@ import { Link } from "react-router-dom";
 export const FooterContainer = styled.footer`
   background-color: #0d0909;
 `;
+
+export const FooterWrap = styled.div`
+  padding: 16px 24px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  max-width: 1300px;
+  margin: 0 auto;
+`;
